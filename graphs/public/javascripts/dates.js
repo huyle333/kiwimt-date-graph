c4d535b8035816a47a1404c2be6c3b1685be2985
Huy Le <huyle333@yahoo.com>
2014-10-18 02:59:21 -0400

    basic javascript testing on dates

--- graphs/public/javascripts/dates.js
+++ graphs/public/javascripts/dates.js
@@ -1,1 +1,10 @@
 document.write(Date());
+
+window.onload = function () {
+  var select = document.getElementById("year");
+  for(var i = 2014; i >= 1950; i--){
+    var option = document.createElement("option");
+    option.text = option.value = i;
+    select.add(option, 0);
+  }
+};
